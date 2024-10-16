4fb011d4b53852a472a1a26b4c3c052800bb70e0
Curso <Curso@server.ipartekaula.com>
2024-10-16 17:24:55 +0200

    Ejemplo servlet

---
+++ ejemploweb/src/main/java/ejemploweb/servlets/HolaMundo.java
@@ -0,0 +1,30 @@
+package ejemploweb.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hola")
+public class HolaMundo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+
+		PrintWriter out = response.getWriter();
+
+		out.println("<ul>");
+
+		for (int i = 1; i <= 6; i++) {
+			out.println("<li>" + i + "</li>");
+		}
+
+		out.println("</ul>");
+	}
+}
