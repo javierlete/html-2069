60b5349f3e4eaadf3ff192e145dc04ef1043704b
Curso <Curso@server.ipartekaula.com>
2024-10-28 17:10:43 +0100

    Los controladores leen e insertan los mensajes en la base de datos

--- iparchat/src/main/java/iparchat/controladores/IndexServlet.java
+++ iparchat/src/main/java/iparchat/controladores/IndexServlet.java
@@ -2,8 +2,8 @@ package iparchat.controladores;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 
+import iparchat.accesodatos.MensajesAccesoDatos;
 import iparchat.modelos.Mensaje;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -15,31 +15,27 @@ import jakarta.servlet.http.HttpSession;
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static final ArrayList<Mensaje> mensajes = new ArrayList<Mensaje>();
-	
-	static {
-		mensajes.add(new Mensaje(1L, "Pepe", "Ola ke ase", LocalDateTime.now().minusDays(1)));
-		mensajes.add(new Mensaje(2L, "Javier", "Pues nada, aquí dando clase", LocalDateTime.now()));
-	}
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setAttribute("mensajes", mensajes);
-		
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setAttribute("mensajes", MensajesAccesoDatos.getMensajes());
+
 		request.getRequestDispatcher("index.jsp").forward(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String emisor = request.getParameter("emisor");
 		String texto = request.getParameter("texto");
-		
+
 		Mensaje mensaje = new Mensaje(null, emisor, texto, LocalDateTime.now());
-		
-		mensajes.add(mensaje);
-		
-		HttpSession session  = request.getSession();
-		
+
+		MensajesAccesoDatos.mandarMensaje(mensaje);
+
+		HttpSession session = request.getSession();
+
 		session.setAttribute("emisor", emisor);
-		
+
 		response.sendRedirect("index#formulario");
 	}
 
