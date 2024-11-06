a43bd8d6bb41ba15b9b25de224fb0a92667b3bfa
Curso <Curso@server.ipartekaula.com>
2024-11-06 16:29:54 +0100

    perfil.jsp finalizado mostrando datos de servlet que manda simulacros

--- iparovo/src/main/java/iparovo/controladores/PerfilServlet.java
+++ iparovo/src/main/java/iparovo/controladores/PerfilServlet.java
@@ -1,17 +1,29 @@
 package iparovo.controladores;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import iparovo.modelos.Cesta;
+import iparovo.modelos.Restaurante;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @WebServlet("/perfil")
 public class PerfilServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		var pedidos = new ArrayList<Cesta>();
+		
+		pedidos.add(new Cesta(null, null, new Restaurante(null, "Restaurante 1", null, null, null, null, null, null), LocalDateTime.now()));
+		pedidos.add(new Cesta(null, null, new Restaurante(null, "Restaurante 2", null, null, null, null, null, null), LocalDateTime.now()));
+		
+		request.setAttribute("pedidos", pedidos);
+		
 		request.getRequestDispatcher("perfil.jsp").forward(request, response);
 	}
 }
