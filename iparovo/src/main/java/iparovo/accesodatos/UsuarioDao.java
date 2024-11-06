b533694c835eddf5eae99c92eb603556437c8b69
Curso <Curso@server.ipartekaula.com>
2024-11-06 16:48:56 +0100

    UsuarioDao ahora conecta a la base de datos para los pedidos

--- iparovo/src/main/java/iparovo/accesodatos/UsuarioDao.java
+++ iparovo/src/main/java/iparovo/accesodatos/UsuarioDao.java
@@ -17,6 +17,13 @@ import iparovo.modelos.Usuario;
 public class UsuarioDao {
 	private static final String sqlSelectEmail = "SELECT * FROM usuarios WHERE email='%s'";
 	private static final String sqlInsert = "INSERT INTO usuarios (nombre, email, password, rol) VALUES ('%s', '%s', '%s', '%s')";
+	private static final String sqlSelectPedidos = """
+			SELECT p.id, r.nombre, p.fecha 
+			FROM pedidos p
+			JOIN restaurantes r ON p.restaurante_id = r.id
+			WHERE usuario_id = %s
+			ORDER BY fecha DESC
+			""";
 	
 	public static void insertar(Usuario usuario) {
 		String sql = String.format(sqlInsert, usuario.getNombre(), usuario.getEmail(), usuario.getPassword(), usuario.getRol());
@@ -45,13 +52,29 @@ public class UsuarioDao {
 		}
 	}
 
-	public static Object buscarPedidosPorIdUsuario(Long id) {
+	public static ArrayList<Cesta> buscarPedidosPorIdUsuario(Long id) {
 		var pedidos = new ArrayList<Cesta>();
 		
-		pedidos.add(new Cesta(null, null, new Restaurante(null, "Restaurante 1" + id, null, null, null, null, null, null), LocalDateTime.now()));
-		pedidos.add(new Cesta(null, null, new Restaurante(null, "Restaurante 2" + id, null, null, null, null, null, null), LocalDateTime.now()));
-		
-		return pedidos;
+		String sql = String.format(sqlSelectPedidos, id);
+
+		try (Connection con = DriverManager.getConnection(url);
+				Statement st = con.createStatement();
+				ResultSet rs = st.executeQuery(sql)) {
+			
+			Cesta pedido;
+			Restaurante restaurante;
+			
+			while(rs.next()) {
+				restaurante = new Restaurante(null, rs.getString("nombre"), null, null, null, null, null, null);
+				pedido = new Cesta(rs.getLong("id"), null, restaurante, LocalDateTime.parse(rs.getString("fecha")));
+
+				pedidos.add(pedido);
+			}
+			
+			return pedidos;
+		} catch (SQLException e) {
+			throw new RuntimeException("No se ha podido realizar la consulta", e);
+		}
 	}
 
 }
