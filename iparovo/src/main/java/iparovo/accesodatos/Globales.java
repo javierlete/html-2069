8b028633c13f36ff66610033eb94183ac382883f
Curso <Curso@server.ipartekaula.com>
2024-11-07 17:18:27 +0100

    Vuelta a SQLite

--- iparovo/src/main/java/iparovo/accesodatos/Globales.java
+++ iparovo/src/main/java/iparovo/accesodatos/Globales.java
@@ -6,12 +6,13 @@ class Globales {
 	static final String pass;
 	
 	static {
-		url = "jdbc:mysql://localhost:3306/iparovo";
+		url = "jdbc:sqlite:C:\\Users\\curso.IPARTEKAULA\\git\\html-2069\\iparovo\\bdd\\iparovo.db"; //"jdbc:mysql://localhost:3306/iparovo";
 		user = "root";
 		pass = "1234";
 
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			Class.forName("org.sqlite.JDBC");
+			//Class.forName("com.mysql.cj.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			throw new RuntimeException("No se ha encontrado el driver", e);
 		}
